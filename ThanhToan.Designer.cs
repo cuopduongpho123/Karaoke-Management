21e162ab851b2f4148888b702ac5e85a3d63ca13
Family-us <bigrubyzz1606@gmail.com>
2026-05-12 17:03:54 +0700

    Lan 1 chua xong

--- ThanhToan.Designer.cs
+++ ThanhToan.Designer.cs
@@ -28,12 +28,305 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnreset = new System.Windows.Forms.Button();
+            this.btnthanhtoan = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.mahoadon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maphong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.makhachhang = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tenphong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtmahoadon = new System.Windows.Forms.TextBox();
+            this.txtmakhachhang = new System.Windows.Forms.TextBox();
+            this.txtmaphong = new System.Windows.Forms.TextBox();
+            this.txttenphong = new System.Windows.Forms.TextBox();
+            this.txtprice = new System.Windows.Forms.TextBox();
+            this.txttime = new System.Windows.Forms.TextBox();
+            this.btnxoa = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thanh Toán";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tìm Phòng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mã Hóa Đơn";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mã Khách Hàng";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(53, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Mã Phòng";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(53, 270);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Tên Phòng";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(53, 330);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Đơn Giá";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(53, 383);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 16);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Thời Gian";
+            // 
+            // btnreset
+            // 
+            this.btnreset.Location = new System.Drawing.Point(408, 438);
+            this.btnreset.Name = "btnreset";
+            this.btnreset.Size = new System.Drawing.Size(162, 71);
+            this.btnreset.TabIndex = 8;
+            this.btnreset.Text = "Reset";
+            this.btnreset.UseVisualStyleBackColor = true;
+            this.btnreset.Click += new System.EventHandler(this.btnreset_Click);
+            // 
+            // btnthanhtoan
+            // 
+            this.btnthanhtoan.Location = new System.Drawing.Point(937, 438);
+            this.btnthanhtoan.Name = "btnthanhtoan";
+            this.btnthanhtoan.Size = new System.Drawing.Size(162, 71);
+            this.btnthanhtoan.TabIndex = 9;
+            this.btnthanhtoan.Text = "Thanh Toán";
+            this.btnthanhtoan.UseVisualStyleBackColor = true;
+            this.btnthanhtoan.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.mahoadon,
+            this.maphong,
+            this.makhachhang,
+            this.tenphong,
+            this.price,
+            this.time});
+            this.dataGridView1.Location = new System.Drawing.Point(333, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(801, 363);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // mahoadon
+            // 
+            this.mahoadon.HeaderText = "Mã Hóa Đơn";
+            this.mahoadon.MinimumWidth = 6;
+            this.mahoadon.Name = "mahoadon";
+            this.mahoadon.Width = 125;
+            // 
+            // maphong
+            // 
+            this.maphong.HeaderText = "Mã Phòng";
+            this.maphong.MinimumWidth = 6;
+            this.maphong.Name = "maphong";
+            this.maphong.Width = 125;
+            // 
+            // makhachhang
+            // 
+            this.makhachhang.HeaderText = "Mã khách hàng";
+            this.makhachhang.MinimumWidth = 6;
+            this.makhachhang.Name = "makhachhang";
+            this.makhachhang.Width = 125;
+            // 
+            // tenphong
+            // 
+            this.tenphong.HeaderText = "Tên Phòng";
+            this.tenphong.MinimumWidth = 6;
+            this.tenphong.Name = "tenphong";
+            this.tenphong.Width = 125;
+            // 
+            // price
+            // 
+            this.price.HeaderText = "Đơn Giá";
+            this.price.MinimumWidth = 6;
+            this.price.Name = "price";
+            this.price.Width = 125;
+            // 
+            // time
+            // 
+            this.time.HeaderText = "Thời gian";
+            this.time.MinimumWidth = 6;
+            this.time.Name = "time";
+            this.time.Width = 125;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(216, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // txtmahoadon
+            // 
+            this.txtmahoadon.Location = new System.Drawing.Point(219, 107);
+            this.txtmahoadon.Name = "txtmahoadon";
+            this.txtmahoadon.Size = new System.Drawing.Size(100, 22);
+            this.txtmahoadon.TabIndex = 12;
+            this.txtmahoadon.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtmakhachhang
+            // 
+            this.txtmakhachhang.Location = new System.Drawing.Point(216, 161);
+            this.txtmakhachhang.Name = "txtmakhachhang";
+            this.txtmakhachhang.Size = new System.Drawing.Size(100, 22);
+            this.txtmakhachhang.TabIndex = 13;
+            this.txtmakhachhang.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtmaphong
+            // 
+            this.txtmaphong.Location = new System.Drawing.Point(219, 218);
+            this.txtmaphong.Name = "txtmaphong";
+            this.txtmaphong.Size = new System.Drawing.Size(100, 22);
+            this.txtmaphong.TabIndex = 14;
+            // 
+            // txttenphong
+            // 
+            this.txttenphong.Location = new System.Drawing.Point(216, 270);
+            this.txttenphong.Name = "txttenphong";
+            this.txttenphong.Size = new System.Drawing.Size(100, 22);
+            this.txttenphong.TabIndex = 15;
+            // 
+            // txtprice
+            // 
+            this.txtprice.Location = new System.Drawing.Point(216, 330);
+            this.txtprice.Name = "txtprice";
+            this.txtprice.Size = new System.Drawing.Size(100, 22);
+            this.txtprice.TabIndex = 16;
+            this.txtprice.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // txttime
+            // 
+            this.txttime.Location = new System.Drawing.Point(219, 383);
+            this.txttime.Name = "txttime";
+            this.txttime.Size = new System.Drawing.Size(100, 22);
+            this.txttime.TabIndex = 17;
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(656, 438);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(162, 71);
+            this.btnxoa.TabIndex = 18;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            // 
+            // ThanhToan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1146, 521);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.txttime);
+            this.Controls.Add(this.txtprice);
+            this.Controls.Add(this.txttenphong);
+            this.Controls.Add(this.txtmaphong);
+            this.Controls.Add(this.txtmakhachhang);
+            this.Controls.Add(this.txtmahoadon);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnthanhtoan);
+            this.Controls.Add(this.btnreset);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ThanhToan";
             this.Text = "ThanhToan";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnreset;
+        private System.Windows.Forms.Button btnthanhtoan;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mahoadon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maphong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn makhachhang;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tenphong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtmahoadon;
+        private System.Windows.Forms.TextBox txtmakhachhang;
+        private System.Windows.Forms.TextBox txtmaphong;
+        private System.Windows.Forms.TextBox txttenphong;
+        private System.Windows.Forms.TextBox txtprice;
+        private System.Windows.Forms.TextBox txttime;
+        private System.Windows.Forms.Button btnxoa;
     }
 }
--- ThanhToan.cs
+++ ThanhToan.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace laptrinhwin
 {
@@ -15,6 +16,51 @@ namespace laptrinhwin
         public ThanhToan()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(txtmahoadon.Text, txtmaphong.Text , txtmakhachhang.Text ,  txttenphong.Text , txtprice.Text , txttime.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtmahoadon.Text = row.Cells[0].Value.ToString();
+                txtmakhachhang.Text = row.Cells[2].Value.ToString();
+                txtmaphong.Text = row.Cells[1].Value.ToString();
+                txttenphong.Text = row.Cells[3].Value.ToString();
+                txtprice.Text = row.Cells[4].Value.ToString();
+                txttime.Text = row.Cells[5].Value.ToString();
+            }
+        }
+
+        private void btnreset_Click(object sender, EventArgs e)
+        {
+            txtmahoadon.Text = "";
+            txtmakhachhang.Text = "";
+            txtmaphong.Text = "";
+            txtprice.Text = "";
+            txttenphong.Text = "";
+            txttime.Text = "";
+
         }
     }
 }
